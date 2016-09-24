11d4419f4fae321f0fbb25d9b60117aa0d359e40
Josh Hansen <hansen.joshuaa@gmail.com>
2016-09-23 18:41:54 -0600

    Make Log wrap

--- src/ui/log.rs
+++ src/ui/log.rs
@@ -1,5 +1,6 @@
 extern crate termion;
 
+use std::collections::VecDeque;
 use std::io::{StdoutLock,Write};
 
 use termion::event::Key;
@@ -11,16 +12,23 @@ use util::Rect;
 
 pub struct LogArea {
     rect: Rect,
-    messages: Vec<String>
+    messages: VecDeque<String>
 }
 
 impl LogArea {
     pub fn new(rect: &Rect) -> Self {
-        LogArea{ rect: *rect, messages: Vec::new() }
+        LogArea{ rect: *rect, messages: VecDeque::new() }
+    }
+
+    fn max_messages(&self) -> u16 {
+        self.rect.height() - 1
     }
 
     pub fn log_message(&mut self, message: String) {
-        self.messages.push(message);
+        self.messages.push_back(message);
+        if self.messages.len() > self.max_messages() as usize {
+            self.messages.pop_front();
+        }
     }
 }
 
