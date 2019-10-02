7410f170a8b3992c151e597db07fb10f5169da83
Josh Hansen <hansen.joshuaa@gmail.com>
2019-10-02 03:23:22 -0700

    Put FIXMEs and TODOs in milestones leading up to 1.0

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,49 @@
 //! Umpire: Combat Quest of the Millennium
 //!
 
-// FIXME Disallow selecting units on cities where such units make no sense
-// FIXME Don't let aircraft or naval vessels conquer cities
-// FIXME Make it clear when a unit is inside a city
-// FIXME Make Map::draw_tile faster, maybe by eliminating tile cloning
-// FIXME Fix problems with small map sizes: 1) crash if viewport is larger than map 2) limitless wrapping where we should only wrap once
+// 0.4 Milestones
+// TODO Remove CarryOutOrdersMode and CarryOutUnitOrdersMode
 // FIXME Auto-explore mode for units is broken
 //       - the search for the next goal is somtimes really slow
 //       - it happens that the auto-explore will request a move that exceeds a unit's moves remaining
+// FIXME Disallow selecting units on cities where such units make no sense
+// FIXME Don't let aircraft or naval vessels conquer cities
+// TODO Allow examine mode go-to to unobserved tiles, on a best-effort basis
+
+// 0.5 Milestones
 // TODO Fuel limits for aircraft
-// TODO Game save/load
-// TODO Color console text announcing turn start to correspond to player colors?
-// TODO Zoomed-out map view?
-// TODO AI
-// TODO Unit names that better reflect current world naming patterns rather than just the US from 10/20 years ago.
 // TODO Wake up units with auto-explore and go-to orders when they encounter something interesting
 // TODO Wake up sentried units when an enemy comes within their sight.
+// TODO Opening theme music
+// TODO Allow map specification at command-line
+// FIXME Make it clear when a unit is inside a city
+
+// 0.6 Milestones
 // TODO Make splash screen respect color palette
 // FIXME Make splash screen fit the terminal size
-// TODO Allow map specification at command-line
+// FIXME Fix problems with small map sizes: 1) crash if viewport is larger than map 2) limitless wrapping where we should only wrap once
+// TODO Zoomed-out map view?
+// TODO Color console text announcing turn start to correspond to player colors?
+
+// 0.7 Milestones
+// TODO Travis infrastructure
+// TODO Remove all git-based dependencies
+// TODO Decruftification
+// TODO API cleanup
+// TODO Improved test coverage
 // TODO? Move `log` into `ui` and make `Game` fully abstract?
-// TODO Allow examine mode go-to to unobserved tiles, on a best-effort basis
+// TODO Profile and optimize
+
+// 0.8 Milestones
+// TODO Windows support
+// TODO OSX support
+// TODO Game save/load
+
+// 0.9 Milestones
+// TODO Unit names that better reflect current world naming patterns rather than just the US from 10/20 years ago.
+
+// 1.0 Milestones
+// TODO AI
 
 #![allow(clippy::cognitive_complexity)]
 #![allow(clippy::let_and_return)]
