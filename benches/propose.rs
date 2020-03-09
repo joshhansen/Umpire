223d58ad02d3205e225219e3d06dd5c0771969c5
Josh Hansen <hansen.joshuaa@gmail.com>
2020-03-09 16:30:10 -0700

    Fix propose benchmark by isolating the propose method itself

--- benches/propose.rs
+++ benches/propose.rs
@@ -5,14 +5,31 @@ use criterion::Criterion;
 
 use umpire::{
     game::{
-        test_support::test_propose_move_unit_by_id,
+        test_support::game_two_cities_two_infantry,
+        unit::UnitID,
     },
+    util::Location,
 };
 
 fn criterion_benchmark(c: &mut Criterion) {
+    // let src = Location{x:0, y:0};
+    let dest = Location{x:1, y:0};
+
+    let game = game_two_cities_two_infantry();
+
+    let unit_id: UnitID = game.unit_orders_requests().next().unwrap();
+
+    // {
+    //     let unit = game.current_player_unit_by_id(unit_id).unwrap();
+    //     assert_eq!(unit.loc, src);
+    // }
+
+    // let proposed_move = game.propose_move_unit_by_id(unit_id, dest).unwrap();
     c.bench_function(
         "test_propose_move_unit_by_id",
-        |b| b.iter(|| test_propose_move_unit_by_id())
+        |b| b.iter(|| {
+            game.propose_move_unit_by_id(unit_id, dest).unwrap()
+        })
     );
 }
 
