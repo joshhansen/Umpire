6941ece18ccc1b0ed7ab715883df8d0b57a1812f
Josh Hansen <hansen.joshuaa@gmail.com>
2020-03-14 03:14:21 -0700

    A benchmark for RandomAI

---
+++ benches/random_ai.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+
+use umpire::{
+    game::{
+        ai::RandomAI,
+        player::TurnTaker,
+        test_support::game_two_cities_two_infantry,
+    },
+};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut game = game_two_cities_two_infantry();
+
+    let mut ctrl = game.player_turn_control(0);
+    let mut ai = RandomAI::new();
+    c.bench_function(
+        "random_ai",
+        |b| b.iter(|| {
+            ai.take_turn(&mut ctrl);
+        })
+    );
+}
+
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
