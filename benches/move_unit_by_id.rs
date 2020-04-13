58f2b4e339f0603f4b5cdfe08fe202287c5bae52
Josh Hansen <hansen.joshuaa@gmail.com>
2020-04-12 17:38:02 -0700

    A benchmark for move_unit_by_id

---
+++ benches/move_unit_by_id.rs
@@ -0,0 +1,52 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::{
+    BatchSize,
+    Criterion,
+};
+
+use umpire::{
+    game::{
+        Alignment,
+        Game,
+        map::{
+            MapData,
+            terrain::Terrain,
+        },
+        
+        unit::UnitType,
+    },
+    util::{
+        Dims,
+        Location,
+        Vec2d,
+        Wrap2d,
+    },
+};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    
+    // let game = Game::new(Dims::new(190, 80), city_namer, 4, false, None, Wrap2d::BOTH);
+
+    c.bench_function(
+        "move_unit_by_id",
+        |b| b.iter_batched_ref(|| {
+            let mut map = MapData::new(Dims::new(180, 90), |_| Terrain::Water);
+            let unit_id = map.new_unit(Location::new(0,0), UnitType::Fighter, Alignment::Belligerent{player:0}, "Han Solo").unwrap();
+
+            let game = Game::new_with_map(map, 1, true, None, Wrap2d::BOTH);
+
+            let unit_loc = game.current_player_unit_by_id(unit_id).unwrap().loc;
+            let dest = game.wrapping().wrapped_add(game.dims(), unit_loc, Vec2d::new(5, 5)).unwrap();
+            (game, unit_id, dest)
+        },
+            |(game,unit_id,dest)| game.move_unit_by_id(*unit_id, *dest).unwrap(),
+            BatchSize::SmallInput,
+        )
+    );
+}
+
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
