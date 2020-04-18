18edd23630d556b2c7fd779542b2b0b8a322ee95
Josh Hansen <hansen.joshuaa@gmail.com>
2020-04-18 06:15:32 -0700

    Fix perf scripts

--- scripts/umpire-ai_train_perf-record-to-id.sh
+++ scripts/umpire-ai_train_perf-record-to-id.sh
@@ -2,4 +2,4 @@
 ID=$1
 cargo build
 set -x
-perf record -g -o profiling/perf/record/umpire-ai_train.$ID target/debug/umpire-ai -e 10 -s 100 -W 10 -H 10 train -a /tmp/profiling_10x10_e100_s100_a.ai
+perf record -g -o profiling/perf/record/umpire-ai_train.$ID target/debug/umpire-ai -v -W 10 -H 10 -e 1 -s 1000 train -a /tmp/profiling_10x10_e1_s1000_a.ai
