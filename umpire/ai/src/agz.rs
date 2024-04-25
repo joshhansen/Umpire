dc28b88383e4d0b1867ee72122329f1f94982465
Josh Hansen <me@joshhansen.tech>
2024-04-25 14:56:16 -0700

    Decruft

--- umpire/ai/src/agz.rs
+++ umpire/ai/src/agz.rs
@@ -198,62 +198,6 @@ impl<B: Backend> DNN<B> {
         out2
     }
 
-    // /// Two variables must be set:
-    // /// * `learning_rate`: the DNN learning rate, f64
-    // /// * `possible_actions`: the number of values to predict among, i64
-    // pub fn new(device: Device, learning_rate: f64, possible_actions: i64) -> Result<Self, String> {
-    //     let path = vars.root();
-
-    //     let convs = vec![
-    //         nn::conv2d(
-    //             &path,
-    //             BASE_CONV_FEATS,
-    //             BASE_CONV_FEATS * 2,
-    //             3,
-    //             Default::default(),
-    //         ), // -> 9x9
-    //         nn::conv2d(
-    //             &path,
-    //             BASE_CONV_FEATS * 2,
-    //             BASE_CONV_FEATS * 2,
-    //             3,
-    //             Default::default(),
-    //         ), // -> 7x7
-    //         nn::conv2d(
-    //             &path,
-    //             BASE_CONV_FEATS * 2,
-    //             BASE_CONV_FEATS * 2,
-    //             3,
-    //             Default::default(),
-    //         ), // -> 5x5
-    //         nn::conv2d(
-    //             &path,
-    //             BASE_CONV_FEATS * 2,
-    //             BASE_CONV_FEATS,
-    //             3,
-    //             Default::default(),
-    //         ), // -> 3x3
-    //     ];
-
-    //     let dense0 = nn::linear(&path, WIDE_LEN + DEEP_OUT_LEN, 64, Default::default());
-    //     let dense1 = nn::linear(&path, 64, 32, Default::default());
-    //     let dense2 = nn::linear(&path, 32, possible_actions, Default::default());
-
-    //     let optimizer = nn::Adam::default()
-    //         .build(&vars, learning_rate)
-    //         .map_err(|err| err.to_string())?;
-
-    //     Ok(Self {
-    //         learning_rate,
-    //         possible_actions,
-    //         convs,
-    //         dense0,
-    //         dense1,
-    //         dense2,
-    //         optimizer,
-    //     })
-    // }
-
     // pub fn train(&mut self, features: &Tensor, action: &usize, value: f64) {
     //     let actual_estimate: Tensor =
     //         self.forward_t(features, true)
@@ -305,65 +249,6 @@ impl<B: Backend> DNN<B> {
     }
 }
 
-// impl StateActionFunction<Game, usize> for DNN {
-//     type Output = f64;
-
-//     fn evaluate(&self, state: &Game, action: &usize) -> Self::Output {
-//         let features = self.tensor_for(state);
-
-//         self.evaluate_tensor(&features, action)
-//     }
-
-//     fn update_with_error(
-//         &mut self,
-//         state: &Game,
-//         action: &usize,
-//         value: Self::Output,
-//         _estimate: Self::Output,
-//         _error: Self::Output,
-//         _raw_error: Self::Output,
-//         _learning_rate: Self::Output,
-//     ) {
-//         let features = self.tensor_for(state);
-
-//         self.train(&features, action, value)
-//     }
-// }
-
-// impl EnumerableStateActionFunction<Game> for DNN {
-//     fn n_actions(&self) -> usize {
-//         AiPlayerAction::possible_actions().len()
-//     }
-
-//     fn evaluate_all(&self, state: &Game) -> Vec<f64> {
-//         (0..self.n_actions())
-//             .map(|action_idx| self.evaluate(state, &action_idx))
-//             .collect()
-//     }
-
-//     fn update_all_with_errors(
-//         &mut self,
-//         state: &Game,
-//         values: Vec<f64>,
-//         estimates: Vec<f64>,
-//         errors: Vec<f64>,
-//         raw_errors: Vec<f64>,
-//         learning_rate: f64,
-//     ) {
-//         for (i, value) in values.iter().enumerate() {
-//             self.update_with_error(
-//                 state,
-//                 &i,
-//                 *value,
-//                 estimates[i],
-//                 errors[i],
-//                 raw_errors[i],
-//                 learning_rate,
-//             );
-//         }
-//     }
-// }
-
 impl<B: Backend> Loadable for DNN<B> {
     fn load<P: AsRef<Path>>(path: P) -> Result<Self, String> {
         let path = path.as_ref();
@@ -379,52 +264,6 @@ impl<B: Backend> Loadable for DNN<B> {
         Self::load_from_bytes(r)
     }
 }
-
-// impl nn::ModuleT for DNN {
-//     fn forward_t(&self, xs: &Tensor, train: bool) -> Tensor {
-//         let split: Vec<Tensor> = xs.split_with_sizes(&[WIDE_LEN, DEEP_LEN], 0);
-
-//         // Wide featuers that will pass through to the dense layers directly
-//         let wide = &split[0];
-
-//         // Input features to the 2d convolution
-//         let mut deep = split[1].view([1, BASE_CONV_FEATS, DEEP_WIDTH, DEEP_HEIGHT]);
-
-//         for conv in &self.convs {
-//             deep = deep.apply(conv).relu().dropout(0.4, train);
-//         }
-
-//         // Reshape back to vector
-//         deep = deep.view([-1]);
-
-//         let wide_and_deep = Tensor::cat(&[wide, &deep], 0);
-
-//         debug_assert!(wide_and_deep.device().is_cuda());
-
-//         // println!("Wide and deep shape: {:?}", wide_and_deep.size());
-
-//         // xs.view([-1, 1, 28, 28])
-//         //     .apply(&self.conv1)
-//         //     .max_pool2d_default(2)
-//         //     .apply(&self.conv2)
-//         //     .max_pool2d_default(2)
-//         //     .view([-1, 1024])
-//         //     .apply(&self.fc1)
-//         //     .relu()
-//         //     .dropout_(0.5, train)
-//         //     .apply(&self.fc2)
-
-//         wide_and_deep
-//             .apply(&self.dense0)
-//             .relu()
-//             // .dropout_(0.2, train)
-//             .apply(&self.dense1)
-//             .relu()
-//             // .dropout_(0.2, train)
-//             .apply(&self.dense2)
-//             .softplus()
-//     }
-// }
 
 impl<B: Backend> Storable for DNN<B> {
     fn store(self, path: &Path) -> Result<(), String> {
