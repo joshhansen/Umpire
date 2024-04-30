b720de7652df8bdfed60e2a78437f67359a09e60
Josh Hansen <me@joshhansen.tech>
2024-04-30 13:46:40 -0700

    Tidy up

--- umpire/ai/src/main.rs
+++ umpire/ai/src/main.rs
@@ -630,15 +630,21 @@ async fn main() -> Result<(), String> {
 #[derive(Config)]
 pub struct TrainingConfig {
     pub model: AgzActionModelConfig,
+
     pub optimizer: AdamConfig,
+
     #[config(default = 10)]
     pub num_epochs: usize,
+
     #[config(default = 64)]
     pub batch_size: usize,
+
     #[config(default = 4)]
     pub num_workers: usize,
+
     #[config(default = 42)]
     pub seed: u64,
+
     #[config(default = 1.0e-4)]
     pub learning_rate: f64,
 }
