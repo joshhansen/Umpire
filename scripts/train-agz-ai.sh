0877fadf589cdb68b7dea597ee3e08db1364dce1
Josh Hansen <git@joshhansen.tech>
2024-05-01 12:28:29 -0700

    Training script update - giving us 3 / 100 victories :-/

--- scripts/train-agz-ai.sh
+++ scripts/train-agz-ai.sh
@@ -12,10 +12,10 @@ AI="cargo run --release -p umpire-ai --"
 
 # Generate self-play data
 
-$AI -e 10000 -s 1000 -W $WIDTH -H $HEIGHT -F -vv eval rr -P ai/data/$ID.data -p 0.1
+$AI -e 100000 -s 1000 -W $WIDTH -H $HEIGHT -F -v eval rr -P ai/data/$ID.data -p 0.001
 
 # Train AlphaGo Zero-style action-state model
-$AI -e 100 -W $WIDTH -H $HEIGHT -v agztrain -D 0.00001 -o ai/agz/$ID.agz ai/data/$ID.data
+$AI -e 10 -W $WIDTH -H $HEIGHT -v agztrain -D 0.00001 -o ai/agz/$ID.agz ai/data/$ID.data
 
 # Evaluate the new model against a random model
-$AI -e 100 -F -vv -s 1000 -W $WIDTH -H $HEIGHT eval r ./ai/agz/$ID.agz
+$AI -e 100 -F -vv -s 1000 -W $WIDTH -H $HEIGHT eval r ./ai/agz/$ID.agz/model.mpk
