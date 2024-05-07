1848eb20244d3a6befa57f1b08e780911f31c7b8
Josh Hansen <git@joshhansen.tech>
2024-05-07 11:53:06 -0700

    mkdir

--- scripts/datagen.sh
+++ scripts/datagen.sh
@@ -13,4 +13,7 @@ echo "Games per process: $PERPROC"
 echo "Dest dir: $DEST"
 
 set -x
-seq $PROCS | parallel --lb -j $PROCS cargo run --release -p umpire-ai -- -v -e 10000 -s 4000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval rr -P $DEST/10-40_rr_e$PERPROC_s4000_p0.0001.{}.data -p 0.0001
+
+mkdir -p $DEST
+
+seq $PROCS | parallel --lb -j $PROCS cargo run --release -p umpire-ai -- -v -e $PERPROC -s 4000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval rr -P $DEST/10-40_rr_e${PERPROC}_s4000_p0.0001.{}.data -p 0.0001
