3d04c0a7f0b590939032e6a1490f41978360d5d7
Josh Hansen <git@joshhansen.tech>
2024-05-09 13:21:56 -0700

    Script updates

--- scripts/datagen.sh
+++ scripts/datagen.sh
@@ -16,4 +16,4 @@ set -x
 
 mkdir -p $DEST
 
-seq $PROCS | parallel --lb -j $PROCS cargo run --release -p umpire-ai -- -v -e $PERPROC -s 4000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval rr -P $DEST/10-40_rr_e${PERPROC}_s4000_p0.0001.{}.data -p 0.0001
+seq $PROCS | parallel --lb -j $PROCS cargo run --release -p umpire-ai -- -v -e $PERPROC -s 4000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk -P $DEST/10-40_rr00_e${PERPROC}_s4000_p0.0001.{}.data -p 0.0001
