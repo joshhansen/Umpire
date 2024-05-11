e84d64d7854452f9e86b4296281228db114219eb
Josh Hansen <git@joshhansen.tech>
2024-05-10 17:27:48 -0700

    Fix issue where the `seq` output was going to the umpire-ai command line args

--- scripts/datagen.sh
+++ scripts/datagen.sh
@@ -5,6 +5,8 @@ PROCS=$2
 DEST=$3
 shift; shift; shift;
 
+PROFILE="debug"
+
 PERPROC=`expr $GAMES / $PROCS`
 
 echo "Games: $GAMES"
@@ -16,4 +18,6 @@ set -x
 
 mkdir -p $DEST
 
-seq $PROCS | parallel --lb -j $PROCS cargo run --release -p umpire-ai -- -v -e $PERPROC -s 4000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk -P $DEST/10-40_rr00_e${PERPROC}_s4000_p0.0001.{}.data -p 0.0001
+# ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk 
+# -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40
+seq $PROCS | parallel -j $PROCS --lb -n0 cargo run -p umpire-ai -- -v -e $PERPROC -s 4000 -W 90 -H 45 eval -p 0.0001 rr
