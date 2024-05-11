1400dfeb47f80d1bd843327911a8d7198394b1eb
Josh Hansen <git@joshhansen.tech>
2024-05-10 17:30:55 -0700

    Use maximum stack depth, release-dbg

--- scripts/umpire-ai_eval_rr00_perf-record-to-id.sh
+++ scripts/umpire-ai_eval_rr00_perf-record-to-id.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ID=$1
-PROFILE=debug
-cargo build -p umpire-ai
+PROFILE=release-dbg
+cargo build --profile=$PROFILE -p umpire-ai
 set -x
-perf record --call-graph dwarf -o profiling/perf/record/umpire-ai_eval_rr00.$ID target/$PROFILE/umpire-ai -e 10 -s 1000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk
+perf record --call-graph dwarf,65528 -o profiling/perf/record/umpire-ai_eval_rr00.$ID target/$PROFILE/umpire-ai -e 10 -s 1000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk
