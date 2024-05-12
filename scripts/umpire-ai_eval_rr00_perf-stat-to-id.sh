e15d6f4d8c5505dbc0e39c3a992a05898363612b
Josh Hansen <git@joshhansen.tech>
2024-05-11 17:04:01 -0700

    Fix random seed in profiling

--- scripts/umpire-ai_eval_rr00_perf-stat-to-id.sh
+++ scripts/umpire-ai_eval_rr00_perf-stat-to-id.sh
@@ -3,4 +3,4 @@ ID=$1
 PROFILE=release-dbg
 cargo build --profile $PROFILE -p umpire-ai
 set -x
-perf stat record -d -o profiling/perf/stat/umpire-ai_eval_rr00.$ID target/$PROFILE/umpire-ai -e 10 -s 1000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk
+perf stat record -d -o profiling/perf/stat/umpire-ai_eval_rr00.$ID target/$PROFILE/umpire-ai -e 10 -s 1000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval -S 2938 r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk
