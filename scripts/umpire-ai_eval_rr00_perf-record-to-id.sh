1d5f854812b0e31fc2197d0411d5e822e7145fc2
Josh Hansen <git@joshhansen.tech>
2024-05-14 13:39:15 -0700

    Make perf record script deterministic

--- scripts/umpire-ai_eval_rr00_perf-record-to-id.sh
+++ scripts/umpire-ai_eval_rr00_perf-record-to-id.sh
@@ -3,4 +3,4 @@ ID=$1
 PROFILE=release-dbg
 cargo build --profile=$PROFILE -p umpire-ai
 set -x
-perf record --call-graph dwarf,65528 -o profiling/perf/record/umpire-ai_eval_rr00.$ID target/$PROFILE/umpire-ai -e 10 -s 1000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval -S 8393423  r r ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk
+perf record --call-graph dwarf,65528 -o profiling/perf/record/umpire-ai_eval_rr00.$ID target/$PROFILE/umpire-ai -e 10 -s 1000 -W 10 -W 20 -W 30 -W 40 -H 10 -H 20 -H 30 -H 40 eval -S 2938 --detsec r4343 r8989 ./ai/agz/15x15/0.agz.mpk ./ai/agz/15x15/0.agz.mpk
