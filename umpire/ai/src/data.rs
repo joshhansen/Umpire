37df8c36d860d9f956679194bc87a85d79d2335e
Josh Hansen <git@joshhansen.tech>
2024-06-06 11:32:18 -0700

    Drop erroneous text

--- umpire/ai/src/data.rs
+++ umpire/ai/src/data.rs
@@ -53,10 +53,6 @@ impl<B: Backend> AgzBatcher<B> {
 
 /**
  * A batch of AlphaGo Zero style state-action victory probabilities.
- *
- * The features here include at the end an "action feature" - a way of encoding the action taken.
- *
- * This should be used in training to target the correct output of the model and update only based on that gradient.
 */
 #[derive(Clone, Debug)]
 pub struct AgzBatch<B: Backend> {
